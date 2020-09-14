b4fbd0d33eb3831e6d0010ef128639f0ae909184
ajalsingh <ajal.singh@multifin.com.au>
2020-09-15 00:00:59 +1000

    week 4

--- Week 4/tute4.m
+++ Week 4/tute4.m
@@ -1,9 +1,14 @@
 clear;
+%% Q1
 close all;
 clc;
 
 bag = rosbag('tutorial4.bag');
-%%
+
 laserScan = select(bag, 'Topic', '/scan');
 firstScanMessage = readMessages(laserScan, 1);
-cartesian = readC
+cartesian = firstScanMessage{1}.readCartesian;
+plot(cartesian(:,1),cartesian(:,2))
+
+%% Q2
+
